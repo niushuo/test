8cf898b9804299c49b192056120b0f2d9236c3fe
nss <ns930327@163.com>
2021-07-21 16:52:40 +0800

    上传新的Haha.java文件

---
+++ src/test/Haha.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.uima.cas.StringArrayFS;
+
+import com.rometools.rome.feed.atom.Entry;
+
+public class Haha {
+	public static void main(String[] args) {
+		int[] aa = new int[3];
+		aa[0] = 5;
+		aa[1] = 1111;
+		aa[2] = 10;
+		int x = aa[0];
+		for(int i=1;i<aa.length;i++){
+            if(aa[i]<  x){
+                x=aa[i];
+            }
+        }
+		System.out.println(x );
+		System.out.println(x);
+		}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
